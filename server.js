c70707ac57567d0c1918a8fdcee73dc673a8c1e1
Shaheed mon <sheheedk07@gmail.com>
2025-04-22 09:30:05 +0530

    ongoing

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 await connectDb();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://advique.netlify.app/", 
     credentials: true,
 }));
 
